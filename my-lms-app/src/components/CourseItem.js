07e2afb602960f012345d85c01aa29457e1ec3ae
Charlie Lang <charlie.lang@ucalgary.ca>
2025-03-20 16:28:41 -0600

    fix: correct placement of course description in CourseItem component

--- my-lms-app/src/components/CourseItem.js
+++ my-lms-app/src/components/CourseItem.js
@@ -13,8 +13,8 @@ const CourseItem = ({ course, onEnroll }) => {
       <img src={course.image} alt={course.name} />
       <h3>{course.name}</h3>
       <p>{course.instructor}</p>
-      {showDescription && <p>{course.description}</p>}
       <button onClick={() => onEnroll(course)}>Enroll Now</button>
+      {showDescription && <p>{course.description}</p>}
     </div>
   );
 };
