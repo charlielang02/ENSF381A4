64e5b935af1de65ba402ae1db27d90af3e4645b3
Justin <justin.vuong6@gmail.com>
2025-03-25 14:33:14 -0600

    fixed buttons not showing

--- my-lms-app/src/components/CourseItem.js
+++ my-lms-app/src/components/CourseItem.js
@@ -12,6 +12,8 @@ const CourseItem = ({ course, onEnroll }) => {
     >
       <img src={course.image} alt={course.name} />
       <h3>{course.name}</h3>
+      <p>{course.instructor}</p>
+      <button onClick={() => onEnroll(course)}>Enroll Now</button>
       <p className={`course-description ${showDescription ? "visible" : ""}`}>
         {course.description}
       </p>
